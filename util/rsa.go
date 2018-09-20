74d701f3339a8e6972845a7279026f88d76a6913
yuanxue.yang <yuanxue.yang@blemobi.com>
2018-09-20 10:36:32 +0800

    修复rsa: 私钥加密公钥解密情况

--- util/rsa.go
+++ util/rsa.go
@@ -5,13 +5,19 @@
  Author  : yanue
 
  - 对称加密解密: 服务端保存私钥(和公钥),客户端只拥有公钥
+
  - 使用场景:
    1. cli 公钥加密  						=> svr 私钥解密(RsaPrivateDecrypt)
    2. svr 私钥加密 (RsaPrivateEncrypt) 	=> cli 公钥解密
+
  - 服务端只关心函数: RsaPrivateEncrypt,RsaPrivateDecrypt
 
+ - 分片加密解密: (2048bit)
+   加密时分片长度: pri.PublicKey.N.BitLen()/8-11 = 245
+   解密时分片长度: pri.PublicKey.N.BitLen()/8    = 256
+
  - openssl生成私钥
-	openssl genrsa -out rsa_private_key.pem 1024
+	openssl genrsa -out rsa_private_key.pem 2048
 
  - openssl生成公钥
 	openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
@@ -29,26 +35,37 @@ import (
 	"encoding/pem"
 	"errors"
 	"fmt"
-	"math/big"
 )
 
 // 可通过openssl产生
-//openssl genrsa -out rsa_private_key.pem 1024
+//openssl genrsa -out rsa_private_key.pem 2048
 var privateKey = []byte(`
 -----BEGIN RSA PRIVATE KEY-----
-MIICXAIBAAKBgQDAV7367zhejx2JkYiGzQpS+NON4RSZ3cK9BqyphRws6H2F2pWY
-wpIXMQqtMPCDoOqYHcehzj77CD3PzF4tlKaGByg/oKGXkA9yvI/qygoRMyYadN3c
-CKpe3gYt3glkfg7bmfrA9N15K0wI0UZ3O2xStCGVAeGGhupBP4Tf88r2vwIDAQAB
-AoGABsYQRroN/iaEH8gkSrsF1g41RRXcJ98LcKS/h+jMKTi20vPzuMGBa5eqjJbg
-oYIVQO4rjaM5zJVpt9u2pVxK0PXGK9uA6vnMqZ6YoX9xk2WmT7mLHooB3NwblUn4
-wTuvznWGgd7v8COSg4JUBu1IiefgemzCblYW0f/VMQ8XzQECQQD77BcAy49RJh2H
-F0BV3rUmpHyrU5be5HrdKOrs25zUNx/mKoolbgz+K6BDl1uPxeHFccXUsT6mYfAg
-5/D7am8fAkEAw3TEnKTz+qAw6OjnrCzRtVtO1FEG7IBcBUB9VaqKYy7mJbr8Fcdf
-XTFeswEMcLNly18/u4NZH2frOVK9sNmkYQJAL+NfNA19/uUJ8+YdmrUtJl1aPY80
-PMad/HCMx92vYD/iVnR7skXLPn24h2C8TQZGtqu7+YR/7kzrwrWUf5Zp7QJBAKhw
-Upej19YegsfVHwHTxg2CWJbEFTFvFN45y0kuJQCAhDnzwBaMsHRBfZjJyIy/LXRr
-6yKPeRiFl8LYuTxU80ECQFYQc4M/XdS4BpTX2M0PmktS8XBqfENMPGm6OTRrGZdF
-xu04jk+yaFQPX9dgbkuNX1bm7p11z/tHtWgXE4gc580=
+MIIEowIBAAKCAQEArZ2Uc3jYTB+XS3NfWAaNWbT/5LXbKnd5N7gtDN0PrMf8GlDI
+f8Rf60zJRPD7lfGOnVhVJgHzLpxkq/NlNWb0rTQQ5RHt71mmiGLTiPlM6OLeB4g+
+fAHjRyWf0ijbRnX0tVkRlDOWllcSYHFy0jrTVJp9foTHcntYqnk5a0Q7/S+IGEBr
+SyBVbfgkthjPCKEI1mTxc07OOQhtCzo7pDI98D9mD7Yx28NYIxOzeISYCS+VqWrk
+plaI1uWUNE/nNhh2qYffg3Lf50I8UnOcHGesTjmnUskUfW00dRCVlgGFLilrptuL
+OUbXcaqbdkkLreqlMH43j5WogDFRGhT6GX3FrwIDAQABAoIBAGs6gT6Ua5sQg+Qw
+3LlESrcWKFn8y+E9qxtz4DcqrYy8c4NZU4w+IDP21/SBlhF1AO1DakuwNp8aLr9Y
+87B45zO2jZy9ZyRGTam8yAO4Xf0UaadjSZxTmikOHtixLUUmgz4iRc4v0pkGLC5u
+w0j+1hlR1aJ3pauabRfVCVar7RUg6p0ZkZmnTBMalRKpHY8Gzgb66oA4v/A56VFK
+4cIy/pIV49VHdE5djWuDW/BDJvuv8Y/xhgWp4XGKjRyayRrcDSAHy0T5rdYg/H84
+qROTglfaEY4MJxMhYrPD2GfO7MUSgB545MwqpRA8E7XioGmRBEwNYyaOAaPZV9A0
+1eJOoeECgYEA26ZGkalZfVBCmDCl0HWPqYRO3ehLm6u0+r0NiS59aG7mwlWBUmDU
+1X67O9yKBuj1A7h6EGUQnhi1mKcD4fy9r2mMCNMC0rGS+f27VmLUhFst23KLVPz6
+QvN2YmrdPcnvxbUHtyNTsSZLdZL/H99hS5yK+GY9yqKMt96soMGzI/8CgYEAylkE
+ndAKq7PLKkru2OeGJIj4pr8p07V01J0D+qkiEtcsmIALPAxCPicWs45ZRqLRtPyD
+oRAJcM8GA9Eywwogw1dAnDau/Roq4BikQm9Dff42cmBHIGr2JDp6nsOzfS6cKjxt
+AHIqayt7dQir1H4/5akdPZsj8M1wgu83tmBonlECgYB0h41u38qTWg5KkZyWsJgM
+Fh6FSiU6rGjykXPp8Jkl25hfR1+5pZekwHxy8Ljlm5fJZoiTxBqB1ZgaKZk8voqf
+0j4xvEkGIKFaMYu+8+XNZlY401cqOqBG/sUyx4Eis8yaNkWmmn5fQHLOKLNjZG5I
+3/82c3+azowbTG6HRtxUXwKBgB5vDxuxS7mRfDArPwtOn0VleIiT3fWiqCTGTO/p
+el99D48MSyRH77qrZGWzNkhCeuoOxLl30QOvj4cJcuoU3uKif+w+6UjWI7a63hHD
+7FHJ52SCiJAeplDCnui8JIXiech8eCSGB01BJ/ttR3LZXkDrk6NNbzVroM2Ar091
+5qZRAoGBAKcByMleCzYvrrxZ6vVIg/MI6yjxiypOqUc/kcrzn4gY3j/sszPoqLjX
+4ZfHICipLwWHv4fUvu+5z+RgSQ51cNxZCyMgL+23h9CnkpkvN/ZOUbYHNbn5pzJS
+EwNHBgYK/oZ8+uxjulmA+kNEaq+1kuUq1d/WDCBD+5tXYYVoTZcU
 -----END RSA PRIVATE KEY-----
 `)
 
@@ -56,10 +73,13 @@ xu04jk+yaFQPX9dgbkuNX1bm7p11z/tHtWgXE4gc580=
 //openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
 var publicKey = []byte(`
 -----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDAV7367zhejx2JkYiGzQpS+NON
-4RSZ3cK9BqyphRws6H2F2pWYwpIXMQqtMPCDoOqYHcehzj77CD3PzF4tlKaGByg/
-oKGXkA9yvI/qygoRMyYadN3cCKpe3gYt3glkfg7bmfrA9N15K0wI0UZ3O2xStCGV
-AeGGhupBP4Tf88r2vwIDAQAB
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArZ2Uc3jYTB+XS3NfWAaN
+WbT/5LXbKnd5N7gtDN0PrMf8GlDIf8Rf60zJRPD7lfGOnVhVJgHzLpxkq/NlNWb0
+rTQQ5RHt71mmiGLTiPlM6OLeB4g+fAHjRyWf0ijbRnX0tVkRlDOWllcSYHFy0jrT
+VJp9foTHcntYqnk5a0Q7/S+IGEBrSyBVbfgkthjPCKEI1mTxc07OOQhtCzo7pDI9
+8D9mD7Yx28NYIxOzeISYCS+VqWrkplaI1uWUNE/nNhh2qYffg3Lf50I8UnOcHGes
+TjmnUskUfW00dRCVlgGFLilrptuLOUbXcaqbdkkLreqlMH43j5WogDFRGhT6GX3F
+rwIDAQAB
 -----END PUBLIC KEY-----
 `)
 
@@ -262,86 +282,4 @@ func packageData(originalData []byte, packageSize int) (r [][]byte) {
 	}
 
 	return r
-}
-
-// pure-go realization of RSA_public_decrypt
-func rsaPublicDecrypt(pubKey *rsa.PublicKey, data []byte) ([]byte, error) {
-	c := new(big.Int)
-	m := new(big.Int)
-	m.SetBytes(data)
-	e := big.NewInt(int64(pubKey.E))
-	c.Exp(m, e, pubKey.N)
-	out := c.Bytes()
-	skip := 0
-	for i := 2; i < len(out); i++ {
-		if i+1 >= len(out) {
-			break
-		}
-		if out[i] == 0xff && out[i+1] == 0 {
-			skip = i + 2
-			break
-		}
-	}
-
-	return out[skip:], nil
-}
-
-// pure-go realization of RSA_private_encrypt
-func rsaPrivateEncrypt(priv *rsa.PrivateKey, data []byte) (enc []byte, err error) {
-	k := (priv.N.BitLen() + 7) / 8
-	tLen := len(data)
-	// rfc2313, section 8: The length of the data D shall not be more than k-11 octets
-	if tLen > k-11 {
-		err = fmt.Errorf("input size")
-		return
-	}
-	em := make([]byte, k)
-	em[1] = 1
-	for i := 2; i < k-tLen-1; i++ {
-		em[i] = 0xff
-	}
-	copy(em[k-tLen:k], data)
-	c := new(big.Int).SetBytes(em)
-	if c.Cmp(priv.N) > 0 {
-		err = fmt.Errorf("encryption")
-		return
-	}
-	var m *big.Int
-	var ir *big.Int
-	if priv.Precomputed.Dp == nil {
-		m = new(big.Int).Exp(c, priv.D, priv.N)
-	} else {
-		// We have the precalculated values needed for the CRT.
-		m = new(big.Int).Exp(c, priv.Precomputed.Dp, priv.Primes[0])
-		m2 := new(big.Int).Exp(c, priv.Precomputed.Dq, priv.Primes[1])
-		m.Sub(m, m2)
-		if m.Sign() < 0 {
-			m.Add(m, priv.Primes[0])
-		}
-		m.Mul(m, priv.Precomputed.Qinv)
-		m.Mod(m, priv.Primes[0])
-		m.Mul(m, priv.Primes[1])
-		m.Add(m, m2)
-
-		for i, values := range priv.Precomputed.CRTValues {
-			prime := priv.Primes[2+i]
-			m2.Exp(c, values.Exp, prime)
-			m2.Sub(m2, m)
-			m2.Mul(m2, values.Coeff)
-			m2.Mod(m2, prime)
-			if m2.Sign() < 0 {
-				m2.Add(m2, prime)
-			}
-			m2.Mul(m2, values.R)
-			m.Add(m, m2)
-		}
-	}
-
-	if ir != nil {
-		// Unblind.
-		m.Mul(m, ir)
-		m.Mod(m, priv.N)
-	}
-	enc = m.Bytes()
-	return
 }
--- util/rsa_test.go
+++ util/rsa_test.go
@@ -18,19 +18,30 @@ import (
 var plainTxt = `
 start.
 SA加解密中必须考虑到的密钥长度、明文长度和密文长度问题。明文长度需要小于密钥长度，而密文长度则等于密钥长度。因此当加密内容长度大于密钥长度时，有效的RSA加解密就需要对内容进行分段。
+对应2048位密钥时,加密时分片长度是245,解密时分片长度是256
 end.
 `
 
-func TestRsaSign(t *testing.T) {
-	a, err := RsaSign([]byte(plainTxt))
+func TestRsaEncryptPrivate(t *testing.T) {
+	// 私钥加密,公钥解密
+	a, err := RsaEncryptPrivate([]byte(plainTxt))
 	fmt.Println("a", err)
 	b, err := RsaDecryptPublic(a)
 	fmt.Println("b", err, string(b))
 }
 
-func TestRsaSign1(t *testing.T) {
+func TestRsaEncryptPublic(t *testing.T) {
+	// 公钥加密,私钥解密
 	a, err := RsaEncryptPublic([]byte(plainTxt))
 	fmt.Println("a", err)
 	b, err := RsaDecryptPrivate(a)
 	fmt.Println("b", err, string(b))
+}
+
+func TestRsaSign(t *testing.T) {
+	// 私钥签名,公钥验证
+	a, err := RsaSign([]byte(plainTxt))
+	fmt.Println("a", err)
+	b := RsaSignVerify([]byte(plainTxt), a)
+	fmt.Println("b", b)
 }
